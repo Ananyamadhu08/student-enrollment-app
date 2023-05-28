f528487e6f4c5a2ed7f5809bfd7b80bb3132db5c
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-05-29 04:02:57 +0530

    feat: created pages

---
+++ src/pages/EnrollStudentPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EnrollStudentPage() {
+  return (
+    <div>
+      <h1>Enroll Student Page</h1>
+    </div>
+  );
+}
+
+export default EnrollStudentPage;
---
+++ src/pages/EnrolledStudentPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EnrolledStudentPage() {
+  return (
+    <div>
+      <h1>Enrolled Student Page</h1>
+    </div>
+  );
+}
+
+export default EnrolledStudentPage;
